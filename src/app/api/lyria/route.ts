423de6b5f912e9af61f089f14ce087f18df3b9e0
qianlizeguo <1723877247@qq.com>
2026-03-09 14:08:42 +0800

    add

---
+++ src/app/api/lyria/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse, NextRequest } from "next/server";
+import { cookies } from 'next/headers'
+import { DEFAULT_MODEL, sunoApi } from "@/lib/SunoApi";
+import { corsHeaders } from "@/lib/utils";
+import { lyriaRealtime } from "@/lib/googleGenai";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req: NextRequest) {
+  if (req.method === 'POST') {
+    try {
+      const body = await req.json();
+      const { prompt } = body;
+
+      await lyriaRealtime(prompt);
+
+      return new NextResponse(JSON.stringify({ message: "Audio generated successfully" }), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          ...corsHeaders
+        }
+      });
+    } catch (error: any) {
+      console.error('Error upload:', error);
+      return new NextResponse(JSON.stringify({ error: 'Internal server error: ' + error }), {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+          ...corsHeaders
+        }
+      });
+    }
+  } else {
+    return new NextResponse('Method Not Allowed', {
+      headers: {
+        Allow: 'POST',
+        ...corsHeaders
+      },
+      status: 405
+    });
+  }
+}
+
+
+export async function OPTIONS(request: Request) {
+  return new Response(null, {
+    status: 200,
+    headers: corsHeaders
+  });
+}
---
+++ src/lib/googleGenai.ts
@@ -0,0 +1,57 @@
+import { GoogleGenAI } from "@google/genai";
+import { Buffer } from "buffer";
+import fs from "fs";
+
+const client = new GoogleGenAI({
+  apiKey: process.env.GOOGLE_GENAI_API_KEY || "",
+  apiVersion: "v1alpha" ,
+});
+
+async function lyriaRealtime(textPrompt: string) {
+  // const speaker = new Speaker({
+  //   channels: 2,       // stereo
+  //   bitDepth: 16,      // 16-bit PCM
+  //   sampleRate: 44100, // 44.1 kHz
+  // });
+
+  const session = await client.live.music.connect({
+    model: "models/lyria-realtime-exp",
+    callbacks: {
+      onmessage: (message) => {
+        if (message.serverContent?.audioChunks) {
+          for (const chunk of message.serverContent.audioChunks) {
+            const mt = chunk.mimeType; 
+            console.log("Received audio chunk with MIME type:", mt);
+            if (chunk.data) {
+              console.log("Chunk data length (base64):", chunk.data.length);
+              const audioBuffer = Buffer.from(chunk.data, "base64");
+              fs.writeFileSync("output.raw", audioBuffer); // Save raw audio for debugging
+            }
+            // speaker.write(audioBuffer);
+          }
+        }
+      },
+      onerror: (error) => console.error("music session error:", error),
+      onclose: () => console.log("Lyria RealTime stream closed."),
+    },
+  });
+
+  await session.setWeightedPrompts({
+    weightedPrompts: [
+      { text: textPrompt, weight: 1.0 },
+    ],
+  });
+
+  await session.setMusicGenerationConfig({
+    musicGenerationConfig: {
+      bpm: 90,
+      temperature: 1.0,
+      // audioFormat: "pcm16",  // important so we know format
+      // sampleRateHz: 44100,
+    },
+  });
+
+  await session.play();
+}
+
+export { lyriaRealtime };
