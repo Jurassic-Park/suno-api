41e4f2b4bcceb9bf7cf70b5ef7b01fb6534416c1
qianlizeguo <1723877247@qq.com>
2026-03-09 15:26:18 +0800

    fix

--- src/app/api/lyria/route.ts
+++ src/app/api/lyria/route.ts
@@ -1,9 +1,8 @@
 import { NextResponse, NextRequest } from "next/server";
-import { cookies } from 'next/headers'
-import { DEFAULT_MODEL, sunoApi } from "@/lib/SunoApi";
 import { corsHeaders } from "@/lib/utils";
-import { lyriaRealtime } from "@/lib/googleGenai";
 import fs from "fs";
+import { GoogleGenAI } from "@google/genai";
+import { Buffer } from "buffer";
 
 export const dynamic = "force-dynamic";
 
@@ -13,7 +12,62 @@ export async function POST(req: NextRequest) {
       const body = await req.json();
       const { prompt } = body;
 
-      await lyriaRealtime(prompt);
+      const client = new GoogleGenAI({
+        apiKey: process.env.GOOGLE_GENAI_API_KEY || "",
+        apiVersion: "v1alpha" ,
+      });
+
+      const session = await client.live.music.connect({
+        model: "models/lyria-realtime-exp",
+        callbacks: {
+          onmessage: (message) => {
+            console.log("Received message:", message);
+            if (message.serverContent?.audioChunks) {
+              for (const chunk of message.serverContent.audioChunks) {
+                const mt = chunk.mimeType; 
+                console.log("Received audio chunk with MIME type:", mt);
+                if (chunk.data) {
+                  console.log("Chunk data length (base64):", chunk.data.length);
+                  const audioBuffer = Buffer.from(chunk.data, "base64");
+                  fs.writeFileSync("output.raw", audioBuffer); // Save raw audio for debugging
+                }
+                // speaker.write(audioBuffer);
+              }
+            }
+          },
+          onerror: (error) => console.error("music session error:", error),
+          onclose: () => console.log("Lyria RealTime stream closed."),
+        },
+      });
+
+      await session.setWeightedPrompts({
+        weightedPrompts: [
+          { text: prompt, weight: 1.0 },
+        ],
+      });
+
+      await session.setMusicGenerationConfig({
+        musicGenerationConfig: {
+          bpm: 90,
+          temperature: 1.0,
+          // audioFormat: "pcm16",  // important so we know format
+          // sampleRateHz: 44100,
+        },
+      });
+
+      session.play();
+      
+      try {
+        // Wait for the session to complete or set a timeout
+        await new Promise((resolve, reject) => {
+          const timeout = setTimeout(() => {
+            reject(new Error("Music generation timed out"));
+          }, 60000); // 60 seconds timeout
+        });
+      } catch (error) {
+        console.error('Error during music generation:', error);
+      }
+
 
       return new NextResponse(JSON.stringify({ message: "Audio generated successfully" }), {
         status: 200,
